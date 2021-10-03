a149533c1ccc086ae2cc632a38a16f4e5152d278
BrendenGreeff <brendengreeffie@gmail.com>
2021-10-03 16:18:01 +0200

    Spring boot and swagger done.

---
+++ AccountSystemNew/AccountSystemNewDomain/src/main/java/za/ac/nwu/acnew/domain/service/GeneralResponse.java
@@ -0,0 +1,24 @@
+package za.ac.nwu.acnew.domain.service;
+
+public class GeneralResponse<S> {
+
+    private final boolean successful;
+    //private final transient S payload;
+    private String payload;
+
+
+
+    public GeneralResponse(boolean successful, S payload)
+    {
+        this.successful = true;
+        this.payload = "No Types Found";
+
+    }
+    public boolean getSuccessful() {
+        return successful;
+    }
+    public String getPayload() {
+        return payload;
+    }
+
+}
