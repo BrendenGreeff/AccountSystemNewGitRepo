4d8f42931542d6af8ad45f5f57d4a43cc928251e
BrendenGreeff <brendengreeffie@gmail.com>
2021-10-06 08:57:21 +0200

    Commit after creating AccTypeRepo

---
+++ AccountSystemNew/AccountSystemNewRepository/src/main/java/za/ac/nwu/acnew/repo/persistence/AccountTypeRepository.java
@@ -0,0 +1,10 @@
+package za.ac.nwu.acnew.repo.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.nwu.acnew.domain.persistence.AccountType;
+
+@Repository
+public interface AccountTypeRepository extends JpaRepository<AccountType, Integer> {
+}
+
